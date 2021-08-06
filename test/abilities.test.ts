13ab03d280c85073abb889c1a1cd2b14804c7fae
CAIHUAZHI <huarse@gmail.com>
2021-08-06 17:50:59 +0800

    feat: fetcher 的DELETE请求也支持传入body

--- test/abilities.test.ts
+++ test/abilities.test.ts
@@ -6,7 +6,7 @@ import 'isomorphic-fetch';
 import fetcher from '../src/abilities/fetcher';
 
 test('#fetcher', async () => {
-  const [result1, result2, result3, result4, result5] = await Promise.all([
+  const [result1, result2, result3, result4, result5, result6] = await Promise.all([
     fetcher('https://httpbin.org/get'),
     fetcher('https://httpbin.org/get', {
       data: { id: 1001 },
@@ -24,6 +24,14 @@ test('#fetcher', async () => {
       method: 'DELETE',
       data: { id: 1001 },
     }),
+    fetcher('https://httpbin.org/delete', {
+      method: 'DELETE',
+      data: { id: 1001 },
+      payload: { foo: 'bar' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }),
   ]);
 
   expect(result1).toHaveProperty('headers');
@@ -32,5 +40,5 @@ test('#fetcher', async () => {
   expect(result4.args).toHaveProperty('id', '1001');
   expect(result4).toHaveProperty('data', JSON.stringify({ foo: 'bar' }));
   expect(result5.args).toHaveProperty('id', '1001');
-
+  expect(result6.json).toHaveProperty('foo', 'bar');
 }, 20000);
